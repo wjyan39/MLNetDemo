18bc28a8a4d22be5549812c5e7ca48f03cd517f8
wjyan <17110220033@fudan.edu.cn>
2023-03-02 15:54:56 +0800

    Minor Change in QCInterface.py

--- MyMLNet/OrbNet2/QCInterface.py
+++ MyMLNet/OrbNet2/QCInterface.py
@@ -26,7 +26,7 @@ class OneBodyToSpherical(nn.Module):
         self.register_buffer("out_layout", SphericalTensor.generate_rep_layout_1d_(self.metadata)) 
         self.num_channels_1d = self.metadata.sum().item() 
     
-    def forward(self, feat_ten:torch.Tensor):
+    def forward(self, feat_ten:torch.Tensor) -> SphericalTensor:
         sp_feat_ten = torch.index_select(feat_ten, dim=self.rep_dim, index=self.out_ids) 
         return SphericalTensor(
             sp_feat_ten,
@@ -35,4 +35,25 @@ class OneBodyToSpherical(nn.Module):
             (self.num_channels_1d,),
             (self.rep_layout),
         )
+        
+class TwoBodyToSpherical(nn.Module):
+    def __init__(self, metadata, rep_dims, basis_layout, valence_only=True):
+        """
+        Converting two-body features (order 2 tensor) loaded from QC Calculation to torch_gauge's 2d SphericalTensor.
+        The returned 2d-SO(3) tensor is of symmetric layout along the rep_dims.
+        
+        Note:
+            The layout of two-body feature tensor depends on the basis set employed during QC calculation.
+        """
+        super().__init__()
+        self.metadata = torch.LongTensor(metadata) 
+        self.rep_dims = rep_dims 
+        self.angular_map = {
+            "s": 0,
+            "p": 1,
+            "d": 2,
+            "f": 3,
+            "g": 4, 
+            "h": 5,
+        }
         
